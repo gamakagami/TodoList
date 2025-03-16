449689357d49a7880b422eb6b8cba5ea3c463fbe
Gabriel Anderson <Gab.anderson7779@gmail.com>
2025-03-17 00:48:26 +0700

    final

--- src/firebase.js
+++ src/firebase.js
@@ -9,15 +9,14 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+    apiKey: "AIzaSyDV-ALWRDgtJHhhbJ2VFFUksQIfNX5PCFo",
+    authDomain: "todolist-22c90.firebaseapp.com",
+    projectId: "todolist-22c90",
+    storageBucket: "todolist-22c90.firebasestorage.app",
+    messagingSenderId: "672849462852",
+    appId: "1:672849462852:web:b3372f9522b89e83665e11",
+    measurementId: "G-91VDNQK391",
   };
-
 
 console.log("Firebase API Key:", import.meta.env.VITE_FIREBASE_API_KEY);
 
